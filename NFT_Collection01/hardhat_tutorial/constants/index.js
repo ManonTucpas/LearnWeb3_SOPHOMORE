793ba048ef47ecbb0835540affcce06ba85fdca1
ManonTucpas <manon.lecuyer@gmail.com>
2022-10-18 15:21:28 +0200

    added tokenId

--- NFT_Collection01/hardhat_tutorial/constants/index.js
+++ NFT_Collection01/hardhat_tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x719744e1332d1B0a3b6122549C59aBb42c2b0F4A";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nftcollection-virid.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
